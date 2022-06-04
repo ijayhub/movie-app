f5adbd615477362cb374a712e99ff9874f3209fd
symplyijay <ijeonyi@gmail.com>
2022-06-04 13:16:03 +0100

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Home from './components/Home';
-// import { Routes, Route } from 'react-router-dom'
-// import MovieDetails from './components/MovieDetails';
 
 
-function App() {
+const App=()=> {
 	
 	return (
 		<div className='App'>
